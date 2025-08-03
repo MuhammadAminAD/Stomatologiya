f47c218a428e6e4d26dfdab44b33797028fa9df3
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-08-03 13:27:25 +0500

    universal styles

---
+++ src/styles/index.style.ts
@@ -0,0 +1,5 @@
+export const styles = {
+    Container: "max-w-[1300px] w-18/20 mx-auto",
+    FlexCenter: "flex items-center justify-center",
+    AbsoluteCenter: "absolute top-1/2 left-1/2 -translate-1/2",
+}
