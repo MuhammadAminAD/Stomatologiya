ab7526acef781963a84227e15444a92f75633976
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-08-03 13:34:51 +0500

    components

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../Icons/Logo'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <Logo />
+            </div>
+
+            <div>
+                <ul>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
