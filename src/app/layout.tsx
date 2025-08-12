ae4fdff444a84d632d5e64885360929bad225574
Shukhrat <shuhratshukrillaev123@gmail.com>
2025-08-12 11:47:38 +0500

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "../constants/i18n";
 import I18nProvider from "@/components/I18nProvider";
 import clinicLogo from "../assets/images/logo.png";
+import Script from "next/script";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -84,8 +85,36 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
+        {/* Yandex.Metrika noscript fallback */}
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/103707478" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
+        </noscript>
       </head>
       <body className={`${nunito.variable} antialiased`}>
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103707478', 'ym');
+              ym(103707478, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `
+          }}
+        />
+        
         <I18nProvider>
           {children}
         </I18nProvider>
